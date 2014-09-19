38237e252728c75da8c1f61f05566bab858147e2
Lellansin <lellansin@gmail.com>
2014-09-19 11:26:54 +0800

    update tests

--- test/test.js
+++ test/test.js
@@ -9,12 +9,6 @@ setInterval(function() {
 
 // update file every 3 secs
 setInterval(function() {
-    var data = '{ "name":"' + random(100000, 999999) + '" }';
+    var data = '{ "name":"' + Math.random() + '" }';
     fs.writeFile('./data.json', data);
 }, 3000);
-
-var random = function(min, max) {
-    var range = max - min;
-    var rand = Math.random();
-    return (min + Math.round(rand * range));
-};
